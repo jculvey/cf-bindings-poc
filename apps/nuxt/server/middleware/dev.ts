4fb9da1f0ae3dc56feff21e0d35b2377df5d920a
Dario Piotrowicz <dario@cloudflare.com>
2023-12-12 14:50:01 +0000

    don't use toml file in nuxt

--- apps/nuxt/server/middleware/dev.ts
+++ apps/nuxt/server/middleware/dev.ts
@@ -2,7 +2,14 @@ let bindings: Record<string, unknown>|undefined = undefined;
 
 if(process.env.NODE_ENV === 'development'){
     const { getBindingsProxy } = await import("wrangler");
-    const proxy = await getBindingsProxy();
+    const proxy = await getBindingsProxy({
+        bindings: {
+            MY_KV: {
+                type: 'kv',
+                id: 'xxx',
+            }
+        }
+    });
     bindings = proxy.bindings;
 }
 
