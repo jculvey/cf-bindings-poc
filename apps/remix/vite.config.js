e1505fdce83b27084156b511488f064bdadf418e
Dario Piotrowicz <dario@cloudflare.com>
2023-12-12 14:53:00 +0000

    don't use toml file in remix

--- apps/remix/vite.config.js
+++ apps/remix/vite.config.js
@@ -5,7 +5,14 @@ let env = {};
 
 if(process.env.NODE_ENV === 'development') {
   const { getBindingsProxy } = await import('wrangler');
-  const { bindings } = await getBindingsProxy();
+  const { bindings } = await getBindingsProxy({
+    bindings: {
+      MY_KV: {
+        type: 'kv',
+        id: 'xxx',
+      }
+    }
+  });
   env = bindings;
 }
 
